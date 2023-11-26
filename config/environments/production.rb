3d9f3fc2446e3ed5c58536d404a36ad5df62c4d6
Harir <57466601+Harir99@users.noreply.github.com>
2023-11-26 12:58:59 -0500

    s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present
+  config.action_cable.allowed_request_origins = [/https:\/\/*/]
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
